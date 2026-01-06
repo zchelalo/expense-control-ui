6fdb442ad389e66e679cc7b3d510b55ac7e0197f
zchelalo <eduardosaavedra687@gmail.com>
2026-01-05 21:00:04 -0700

    add: prod Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin()
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'standalone',
 }
 
 export default withNextIntl(nextConfig)
