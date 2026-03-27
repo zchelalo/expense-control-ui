fb770870114df7c1fd8c2e5ab20ac4b0020e7948
zchelalo <eduardosaavedra687@gmail.com>
2026-03-27 11:45:08 -0700

    fix: header @media styles

--- components/templates/landing/header/index.tsx
+++ components/templates/landing/header/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { getTranslations } from 'next-intl/server'
+import { Box } from '@/components/atoms/box'
 import { Button } from '@/components/atoms/button'
+import { FlexBox } from '@/components/atoms/flex-box'
 import { Text } from '@/components/atoms/text'
 import { Title } from '@/components/atoms/title'
 import styles from '@/components/templates/landing/header/header.module.css'
@@ -13,12 +15,37 @@ export async function Header() {
   const { isAuthenticated } = await getAuthSession()
 
   return (
-    <header className={styles.header}>
-      <div className={styles.headerContainer}>
+    <FlexBox
+      variant='header'
+      alignItems='center'
+      justifyContent='center'
+      className={styles.header}
+    >
+      <Box variant='div' className={styles.headerContainer}>
         {/* LEFT SIDE */}
-        <div className={styles.headerContent}>
-          <section className={styles.titleContainer}>
-            <div className={styles.textContainer}>
+        <FlexBox
+          variant='div'
+          direction='column'
+          alignItems='center'
+          justifyContent='center'
+          padding={12}
+          className={styles.headerContent}
+        >
+          <FlexBox
+            variant='section'
+            direction='column'
+            alignItems='start'
+            justifyContent='center'
+            gap={2}
+            className={styles.titleContainer}
+          >
+            <FlexBox
+              variant='div'
+              direction='column'
+              alignItems='start'
+              justifyContent='center'
+              className={styles.textContainer}
+            >
               <Title
                 variant='h1'
                 typographySize='extraLarge4'
@@ -29,9 +56,15 @@ export async function Header() {
               <Text variant='p' typographySize='normal' typographyWeight='bold'>
                 {t('header.subtitle')}
               </Text>
-            </div>
+            </FlexBox>
             {!isAuthenticated ? (
-              <div className={styles.buttonsContainer}>
+              <FlexBox
+                variant='div'
+                alignItems='center'
+                justifyContent='center'
+                gap={4}
+                className={styles.buttonsContainer}
+              >
                 <Link href='/login'>
                   <Button variant='primary' appearance='filled'>
                     {t('header.login_button')}
@@ -42,31 +75,49 @@ export async function Header() {
                     {t('header.signup_button')}
                   </Button>
                 </Link>
-              </div>
+              </FlexBox>
             ) : (
-              <div className={styles.buttonsContainer}>
+              <FlexBox
+                variant='div'
+                alignItems='center'
+                justifyContent='center'
+                gap={4}
+                className={styles.buttonsContainer}
+              >
                 <Link href='/accounts'>
                   <Button variant='primary' appearance='filled'>
                     {t('header.accounts_button')}
                   </Button>
                 </Link>
-              </div>
+              </FlexBox>
             )}
-          </section>
-        </div>
+          </FlexBox>
+        </FlexBox>
 
         {/* RIGHT SIDE */}
-        <div className={styles.headerContent}>
-          <section className={styles.imageContainer}>
+        <FlexBox
+          variant='div'
+          direction='column'
+          alignItems='center'
+          justifyContent='center'
+          padding={12}
+          className={styles.headerContent}
+        >
+          <FlexBox
+            variant='section'
+            alignItems='center'
+            justifyContent='center'
+            className={styles.imageContainer}
+          >
             <Image
               src='/img/chart.svg'
               alt={t('header.chart_illustration_alt')}
               fill
               priority
             />
-          </section>
-        </div>
-      </div>
-    </header>
+          </FlexBox>
+        </FlexBox>
+      </Box>
+    </FlexBox>
   )
 }
