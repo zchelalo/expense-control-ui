ffe670cbb2900ad7a9f3b30da13422df9452cd75
zchelalo <eduardosaavedra687@gmail.com>
2026-04-15 00:34:57 -0700

    feature: use FlexBox component instead of div

--- components/templates/dashboard/accounts/search.tsx
+++ components/templates/dashboard/accounts/search.tsx
@@ -3,8 +3,8 @@
 import { SearchIcon } from 'lucide-react'
 import { useId } from 'react'
 import { Button } from '@/components/atoms/button'
+import { FlexBox } from '@/components/atoms/flex-box'
 import { InputText } from '@/components/atoms/input-text'
-import { Label } from '@/components/atoms/label'
 import styles from '@/components/templates/dashboard/accounts/accounts.module.css'
 import { normalizeAccountSearch } from '@/modules/account/adapters/in/query-params'
 
@@ -20,20 +20,19 @@ export function Search({ translations, search, limit = '10' }: SearchProps) {
   return (
     <form className={styles.form} method='GET'>
       <input type='hidden' name='limit' value={limit} />
-      <div className={styles.formGroup}>
-        <Label htmlFor={searchId}>{translations.searchLabel}</Label>
+      <FlexBox direction='column' gap={2} className={styles.formGroup}>
         <InputText
           id={searchId}
           name='search'
           placeholder={translations.searchPlaceholder}
           defaultValue={normalizeAccountSearch(search)}
         />
-      </div>
-      <div className={styles.formGroup}>
+      </FlexBox>
+      <FlexBox direction='column' gap={2} className={styles.formGroup}>
         <Button type='submit' className={styles.searchButton}>
           <SearchIcon size={18} />
         </Button>
-      </div>
+      </FlexBox>
     </form>
   )
 }
