7e8376f3c356b1dba0ea4124e6317555cbe31abd
zchelalo <eduardosaavedra687@gmail.com>
2026-04-24 11:33:02 -0700

    feature: add missed file

---
+++ modules/movement/ports/movement-store.ts
@@ -0,0 +1,14 @@
+import type { PaginatedResult } from '@/modules/movement/domain/movement-entity'
+
+export type FindAllMovementsFilters = {
+  limit: number
+  afterCursor?: string | null
+  beforeCursor?: string | null
+  accountId?: string | null
+  categoryId?: string | null
+  movementTypeId?: string | null
+}
+
+export interface MovementStore {
+  findAll(filters: FindAllMovementsFilters): Promise<PaginatedResult>
+}
